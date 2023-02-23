6ae5d5dfa8efdfa90f0443c7b5879a8b7ee96bad
kevintanes <kevin.tanes@gmail.com>
2023-02-23 16:25:53 +0700

    setup backend

---
+++ src/migrations/20230223090428-create-products.js
@@ -0,0 +1,54 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      uu_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      stock: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      is_deleted: {
+        type: Sequelize.BOOLEAN
+      },
+      category_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('products');
+  }
+};
---
+++ src/models/products.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class products extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  products.init({
+    uu_id: DataTypes.STRING,
+    name: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    stock: DataTypes.INTEGER,
+    image: DataTypes.STRING,
+    description: DataTypes.STRING,
+    is_deleted: DataTypes.BOOLEAN,
+    category_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'products',
+  });
+  return products;
+};
